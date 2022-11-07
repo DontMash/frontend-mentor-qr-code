b3ccb3a367e6facefc9470f21c08150480b2aab5
Sören Maschmann <smaschmann@slashwhy.de>
2022-11-07 20:42:05 +0100

    Done

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,20 @@
 <script lang="ts">
+  import Footer from './lib/Footer.svelte';
 </script>
 
-<main class="min-h-screen">
-  Improve your front-end skills by building projects
-
-  Scan the QR code to visit Frontend Mentor and take your coding skills to the next level
-  
-  <div class="attribution">
-    Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
-    Coded by <a href="#">Your Name Here</a>.
-  </div>
+<main class="flex justify-center items-center min-h-screen p-6">
+  <h1 class="sr-only">QR Component</h1>
+  <article class="max-w-xs p-4 bg-white rounded-2xl">
+    <picture class="block rounded-xl overflow-hidden">
+      <img src="/images/image-qr-code.png" alt="QR Code" width="576" height="576" />
+    </picture>
+    <div class="p-6 text-center space-y-4">
+      <h2 class="text-xl text-dark-blue font-bold"> Improve your front-end skills by building projects </h2>
+      <p class="text-sm">
+        Scan the QR code to visit Frontend Mentor and take your coding skills to the next
+        level
+      </p>
+    </div>
+  </article>
 </main>
+<Footer />
---
+++ src/lib/Footer.svelte
@@ -0,0 +1,16 @@
+<footer class="p-4 md:p-8">
+  <div class="text-sm text-center">
+    Challenge by <a
+      class="text-dark-blue underline"
+      href="https://www.frontendmentor.io?ref=challenge"
+      target="_blank"
+      rel="noreferrer">Frontend Mentor</a
+    >. Coded by
+    <a
+      class="text-dark-blue underline"
+      href="https://www.github.com/DontMash"
+      target="_blank"
+      rel="noreferrer">DontMash</a
+    >.
+  </div>
+</footer>
